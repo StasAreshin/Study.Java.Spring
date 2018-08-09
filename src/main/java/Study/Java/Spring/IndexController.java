98f52a95cc3628e7260ae210f6edc8ea82cf02e5
Stas <s.areshin@lerpal.com>
2018-08-09 15:48:27 +0300

    Initial commit

---
+++ src/main/java/Study/Java/Spring/IndexController.java
@@ -0,0 +1,22 @@
+package Study.Java.Spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Stas on 2018-08-07.
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        Map<String, String> model = new HashMap<>();
+        model.put("name", "Alexey");
+        return new ModelAndView("index", model);
+    }
+}
